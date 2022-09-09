56b3c8f41cdbfe4230d7c2444dea8bb90e575f9c
sachdevaanisha <sachdeva.anisha@yahoo.com>
2022-09-09 14:19:15 +0200

    project uploaded

---
+++ commands/creator.py
@@ -0,0 +1,4 @@
+import oliviaTTS as ot
+
+def creator(data):
+    ot.speak("Anisha created me on 22nd December 2016")
---
+++ commands/iaf.py
@@ -0,0 +1,4 @@
+import oliviaTTS as ot
+
+def iaf(data):
+   ot. speak("Good to hear that anisha, what can i do for you ?")
